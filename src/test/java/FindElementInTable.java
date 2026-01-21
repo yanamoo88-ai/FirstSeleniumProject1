7967d7a8f0a340224736d085f72fca3024f26e5c
yanam <yanamoo88@gmail.com>
2026-01-21 12:53:32 +0100

    Add

---
+++ src/test/java/FindElementInTable.java
@@ -0,0 +1,64 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FindElementInTable {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp () {
+        driver = new ChromeDriver();
+        driver.get("https://www.w3schools.com/css/css_table.asp");
+        // maximize browser to windows
+        driver.manage().window().maximize();
+        //wait to upload all elements
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver!=null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void findCssSelectorInTable() {
+        // get numbers of rows
+        List<WebElement> rows = driver.findElements(By.cssSelector("tr"));
+        System.out.println(rows.size());
+        for (int i = 0; i < rows.size(); i++) {
+            System.out.println(rows.get(i).getText());
+        }
+        System.out.println("***********************************");
+
+        for (WebElement element: rows) {
+            System.out.println(element.getText());
+        }
+        System.out.println("************************************");
+
+        // get row 2
+        WebElement germany = driver.findElement(By.cssSelector("#customers tr:nth-child(2)"));
+        System.out.println(germany.getText());
+        System.out.println("******************************************");
+
+        // get row 2 and will find 2 element
+        WebElement maria = driver.findElement(By.cssSelector("#customers tr:nth-child(2) td:nth-child(2)"));
+        System.out.println(maria.getText());
+        System.out.println("******************************************");
+
+        //find last child in row
+        //get row 2, last element
+        WebElement last = driver.findElement(By.cssSelector("#customers tr:nth-child(2) td:last-child"));
+        System.out.println(last.getText());
+        System.out.println("*******************************************");
+
+        driver.findElement(By.cssSelector("[type='submit']"));
+        driver.findElement(By.cssSelector(".title-container span.title")); // class + <space> + tag +
+    }
+}
--- src/test/java/FindElementTest.java
+++ src/test/java/FindElementTest.java
@@ -13,8 +13,9 @@ import java.util.List;
 public class FindElementTest {
 
     WebDriver driver;
+
     @BeforeMethod
-    public void setUp () {
+    public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://ilcarro.web.app");
         // maximize browser to windows
@@ -22,45 +23,103 @@ public class FindElementTest {
         //wait to upload all elements
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-   @AfterMethod
-   public void tearDown() {
-        if (driver!=null) {
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
-   }
-   @Test
-    public void findElementByTagName(){
+    }
+
+    @Test
+    public void findElementByTagName() {
         // find one element
         //tag name -> h1
-       WebElement h1 = driver.findElement(By.tagName("h1")); // ybedilis chto text est
-       System.out.println(h1.getText()); // vuveli text
+        WebElement h1 = driver.findElement(By.tagName("h1")); // ybedilis chto text est
+        System.out.println(h1.getText()); // vuveli text
 
-       WebElement h2 = driver.findElement(By.tagName("h2"));
-       System.out.println(h2.getText());
+        WebElement h2 = driver.findElement(By.tagName("h2"));
+        System.out.println(h2.getText());
 
-       WebElement link = driver.findElement(By.tagName("a"));
-       System.out.println(link.getAttribute("class"));
+        WebElement link = driver.findElement(By.tagName("a"));
+        System.out.println(link.getAttribute("class"));
 
 
-       //list
-       List<WebElement> links = driver.findElements(By.tagName("a"));
-       System.out.println(links.size());
-   }
-   @Test
-    public void findElementById(){
+        //list
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+    }
+
+    @Test
+    public void findElementById() {
 // find id
-       WebElement city = driver.findElement(By.id("city"));
-       System.out.println(city.getAttribute("id"));
+        WebElement city = driver.findElement(By.id("city"));
+        System.out.println(city.getAttribute("id"));
 
-       WebElement dates = driver.findElement(By.id("dates"));
-       System.out.println(dates.getAttribute("id"));
-   }
-   @Test
+        WebElement dates = driver.findElement(By.id("dates"));
+        System.out.println(dates.getAttribute("id"));
+    }
+
+    @Test
     public void findElementByClassName() {
-       WebElement telephone = driver.findElement(By.className("telephone"));
-       System.out.println(telephone.getText());
+        WebElement telephone = driver.findElement(By.className("telephone"));
+        System.out.println(telephone.getText());
 
-       WebElement description = driver.findElement(By.className("description"));
-       System.out.println(description.getText());
-   }
+        WebElement description = driver.findElement(By.className("description"));
+        System.out.println(description.getText());
+    }
+
+    @Test
+    public void findElementByLinkText() {
+        WebElement linkText = driver.findElement(By.linkText("Let the car work"));
+        System.out.println(linkText.getText());
+    }
+    @Test
+    public void findElementByPartialLinkText() {
+        WebElement partialLink = driver.findElement(By.partialLinkText("work"));
+        System.out.println(partialLink.getText());
+    }
+    @Test
+    public void findElementByCssSelector() {
+        // driver.findElement(By.tagName("h1"));
+        //tag name "h1" -> scc "h1"
+        driver.findElement(By.cssSelector("h1"));
+
+        //driver.findElement(By.id("city"));
+        //id "city" -> css "#city"
+        driver.findElement(By.cssSelector("#city"));
+
+        //driver.findElement(By.className("telephone"));
+        //class "telephone" -> scc ".telephone"
+        driver.findElement(By.cssSelector(".telephone"));
+
+        //[attr='par']
+        driver.findElement(By.cssSelector("[href='/search']"));
+        driver.findElement(By.cssSelector("[for='city']"));
+
+        //contains -> *
+        driver.findElement(By.cssSelector("[href*='car']"));
+
+        //start -> ^
+        driver.findElement(By.cssSelector("[href^='/let']"));
+
+        //end to -> $
+        driver.findElement(By.cssSelector("[href$='work']"));
+
+        // composite cssSelector
+        driver.findElement(By.cssSelector("a.navigation-link[href='/search']")); // tag + class + pare
+        driver.findElement(By.cssSelector("div.social-networks")); // tag + class
+
+        // step/s below
+        driver.findElement(By.cssSelector(".logo>img")); // > one step below (vniz)
+
+        driver.findElement(By.cssSelector(".feedback .feedback-date")); // <space> one or more steps below
+
+        //super sposob
+        //vubor opredelennogo tovara iz spiska
+        //<tag> or <id> or <class>:nth-child(number)
+        WebElement feedback = driver.findElement(By.cssSelector(".feedback:nth-child(5)"));
+        System.out.println(feedback.getText());
+
+    }
 }
